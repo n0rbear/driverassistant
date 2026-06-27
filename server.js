9a6cbccf05d0f588719690de54bae3cbbc7bb954
n0rbear <horvath.81.norbert@gmail.com>
2026-06-27 03:30:26 +0200

    Update server version from v2 to v3

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// FIXED SERVER v2
+// FIXED SERVER v3
 const express = require('express');
 const { Pool } = require('pg');
 const app = express();
@@ -651,7 +651,7 @@ app.get('/driver/:name', async (req, res) => {
             </script>
         </body>
         </html>
-    \`);
+    `);
 });
 
 const PORT = process.env.PORT || 3000;
